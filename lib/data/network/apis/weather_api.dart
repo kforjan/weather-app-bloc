04e81b27cddc39d00765f7367c4a63cba6557104
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-04-29 18:06:43 +0200

    Weather API and rest client added

---
+++ lib/data/network/apis/weather_api.dart
@@ -0,0 +1,20 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:weather_app_bloc/data/network/rest_client.dart';
+import 'package:weather_app_bloc/models/weather.dart';
+import './weather_api_key.dart';
+
+class WeatherApi {
+  WeatherApi(this._restClient);
+
+  RestClient _restClient;
+
+  Future<Weather> getWeather(Position position) async {
+    try {
+      final response = await _restClient.get(
+          'https://api.openweathermap.org/data/2.5/onecall?lat=${position.latitude}&lon=${position.longitude}&exclude=hourly,daily,minutely&appid=${key}');
+      return Weather.fromJSON(response);
+    } catch (e) {
+      throw e;
+    }
+  }
+}
--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -1,5 +1,25 @@
+import 'package:flutter/foundation.dart';
+
 class Weather {
+  Weather({
+    @required this.temperature,
+    @required this.weather,
+    @required this.watherIcon,
+  });
+
   String temperature;
   String weather;
   String watherIcon;
+
+  factory Weather.fromJSON(Map<String, dynamic> json) {
+    var temperature = double.parse(json['current']['temp']);
+    var temperatureString = ((temperature - 32) * (5 / 9)).toStringAsFixed(1);
+    final weather = json['current']['weather']['main'];
+    final weatherIcon = json['current']['weather']['icon'];
+    return Weather(
+      temperature: temperatureString,
+      weather: weather,
+      watherIcon: weatherIcon,
+    );
+  }
 }
