7cbbf60dbfc541c6c94f1d0d9f706f6430056838
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-05-09 01:05:52 +0200

    Bloc dependency injection fixed

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:weather_app_bloc/blocs/weather_bloc/weather_bloc.dart';
 import 'package:weather_app_bloc/data/network/apis/weather_api.dart';
 import 'package:weather_app_bloc/data/network/rest_client.dart';
 
@@ -7,4 +8,5 @@ final locator = GetIt.instance;
 void setup() {
   locator.registerLazySingleton(() => WeatherApi(locator()));
   locator.registerLazySingleton(() => RestClient());
+  locator.registerFactory(() => WeatherBloc());
 }
